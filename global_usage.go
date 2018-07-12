48a6c007c264d946cd0b34fa9bc4534d8d0903ed
Genevieve Lesperance <glesperance@pivotal.io>
2018-07-12 11:41:24 -0700

    Has usage and commands in global help.
    Signed-off-by: Mejin Leechor <mejin@mejin.codes>

--- global_usage.go
+++ global_usage.go
@@ -6,7 +6,25 @@ import (
 	"strings"
 )
 
-func PrintGlobalUsage(commandSet CommandSet) (string, error) {
+var template = `
+Usage:
+%s
+
+Commands:
+%s
+`
+
+func PrintGlobalUsage(flags string, commandSet CommandSet) (string, error) {
+	var globalFlags []string
+	for _, flag := range strings.Split(flags, "\n") {
+		globalFlags = append(globalFlags, fmt.Sprintf("  %s", flag))
+	}
+
+	flagsUsage := globalFlags[0]
+	for i := 1; i < len(globalFlags); i++ {
+		flagsUsage = fmt.Sprintf("%s\n%s", flagsUsage, globalFlags[i])
+	}
+
 	var (
 		length int
 		names  []string
@@ -25,8 +43,13 @@ func PrintGlobalUsage(commandSet CommandSet) (string, error) {
 	for _, name := range names {
 		command := commandSet[name]
 		name = pad(name, " ", length)
-		commands = append(commands, fmt.Sprintf("%s  %s", name, command.Usage().ShortDescription))
+		commands = append(commands, fmt.Sprintf("  %s  %s", name, command.Usage().ShortDescription))
 	}
 
-	return strings.Join(commands, "\n"), nil
+	commandsUsage := commands[0]
+	for i := 1; i < len(commands); i++ {
+		commandsUsage = fmt.Sprintf("%s\n%s", commandsUsage, commands[i])
+	}
+
+	return fmt.Sprintf(template, flagsUsage, commandsUsage), nil
 }
--- global_usage_test.go
+++ global_usage_test.go
@@ -1,6 +1,7 @@
 package jhanda_test
 
 import (
+	"fmt"
 	"strings"
 
 	. "github.com/onsi/ginkgo"
@@ -9,23 +10,43 @@ import (
 	"github.com/pivotal-cf/jhanda/internal/fakes"
 )
 
+const GLOBAL_USAGE = `
+Usage:
+  --query, -?     asks a question
+  --surprise, -!  gives you a present
+
+Commands:
+  bake   it bakes
+  clean  it cleans
+`
+
 var _ = Describe("Global Usage", func() {
-	It("returns a formatted version of the flag set usage", func() {
+	var (
+		globalFlags string
+		commandSet  jhanda.CommandSet
+	)
+	BeforeEach(func() {
+		globalFlags = strings.TrimSpace(`
+--query, -?     asks a question
+--surprise, -!  gives you a present
+`)
+
 		commandBake := &fakes.Command{}
 		commandBake.UsageReturns(jhanda.Usage{ShortDescription: "it bakes"})
 		commandClean := &fakes.Command{}
 		commandClean.UsageReturns(jhanda.Usage{ShortDescription: "it cleans"})
 
-		commandSet := jhanda.CommandSet{
+		commandSet = jhanda.CommandSet{
 			"bake":  commandBake,
 			"clean": commandClean,
 		}
-
-		usage, err := jhanda.PrintGlobalUsage(commandSet)
+	})
+	It("returns a formatted version of the flag set usage", func() {
+		usage, err := jhanda.PrintGlobalUsage(globalFlags, commandSet)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(usage).To(Equal(strings.TrimSpace(`
-bake   it bakes
-clean  it cleans
-`)))
+
+		fmt.Println(usage)
+
+		Expect(usage).To(Equal(GLOBAL_USAGE))
 	})
 })
